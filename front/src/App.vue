6640c8bc3b1bbaf49930073229aa231e1d77b7e0
peter.pavluchenko <peter.pavluchenko@gmail.com>
2021-03-23 10:56:31 +0100

    Created HomePage in Vue.

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-    <Header></Header>
     <div id="main">
       <router-view />
     </div>
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from "@/views/_common/Header.vue";
-import Footer from "@/views/_common/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    Header,
-    Footer,
   },
 };
 </script>
 
 <style>
-#main {
-  flex: 1;
+body {
+    background-image: linear-gradient(45deg, #78bfa5 25%, #a66363 25%, #a66363 50%, #78bfa5 50%, #78bfa5 75%, #a66363 75%, #a66363 100%);
+    background-size: 56.57px 56.57px;
 }
 </style>
